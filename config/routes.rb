a9a83e5e529ef3fa5a661dfe32f61323acbb04e4
Pedro Cobos <pedro.cobos@live.com>
2022-05-14 10:41:57 -0500

    devise changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  # get 'pages/home'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "pets#index"
+  root "pages#home"
 
   get "pets", to: "pets#index"
   get "pets/new", to: "pets#new"
